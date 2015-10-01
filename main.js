34482351e10ff4ce95edf47bb38dd93b35709bff
Start Coding Student 1 <session1@START-Coding-MBP-C02PP8Q8G8WN.local>
2015-10-01 13:27:55 -0700

    Main skeleton code for game

---
+++ main.js
@@ -0,0 +1,20 @@
+//This is the basic skeleton that lists the start
+var startGame = prompt("The year is YX15. Your nation, the Atrar Union, located on planet Serenity, is being attacked by the Methera Empire. " +
+    "War has broken out as the Atrar soldiers defend your nation against the Methera invaders. Do you help defend at the front lines, " +
+    "try to find the mothership, try to resolve the conflict by speaking with the leader, or flee your planet? (defend/mothership/talk/flee)");
+switch (startGame) {
+    case "defend":
+        break;
+    case "mothership":
+        break;
+    case "talk":
+        break;
+    case "flee":
+        break;
+}
+/* Work on your own part:
+ Andy: Defend
+ Nick: Mothership
+ Marques: Talk
+ Ben: Flee
+ */
